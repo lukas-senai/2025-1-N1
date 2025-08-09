65d4671f5a48887984aae70fc058daec723645d0
lukas-senai <l.ferreira@edu.sc.senai.br>
2025-08-08 21:50:03 -0300

    Update Bajulando o sor kkkk.py

--- Bajulando o sor kkkk.py
+++ Bajulando o sor kkkk.py
@@ -1,12 +1,12 @@
 import time
 
-def efeito_digitacao(texto, delay=0.1):
+def efeito_digitacao(texto, delay=0.2):
     for caractere in texto:
         print(caractere, end='', flush=True)
         time.sleep(delay)
     print()
 
-efeito_digitacao("Prepare-se para ser bajulado kkkkk", 0.1)
+efeito_digitacao("Prepare-se para ser bajulado kkkkkkkkkkkkkkkkkk", 0.1)
 
 print("carregando", end='', flush=True)
 efeito_digitacao("...", 0.4)
@@ -17,6 +17,8 @@ efeito_digitacao("...", 0.4)
 print("carregando", end='', flush=True)
 efeito_digitacao("...", 0.4)
 
-efeito_digitacao("É nóis pa krl sor iiihhhuuuu❤️ ❤️ ❤️", 0.2)
+efeito_digitacao("É nóis pa krl sor iiihhhuuuu❤️ ❤️ ❤️❤️❤️❤️❤️❤️❤️", 0.2)
 
-print("fim do programa")
+print("teste")
+
+print("fim do programa 💫")
